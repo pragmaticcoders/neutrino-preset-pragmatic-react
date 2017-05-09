1bfc0676eeac43a29b3c92aa762f65045f1bbd45
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-05-09 14:34:16 +0200

    example of using `react-hot-loader`

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
 import Hello from './Hello';
 
 
-render(
-  <Hello name="World" />,
-  document.getElementById('root')
-);
+const load = () => render((
+  <AppContainer>
+    <Hello name="World" />
+  </AppContainer>
+), document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept('./Hello', load);
+}
+
+load();
