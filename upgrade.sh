6b26dee31a2571b43381ccaab2df79092567aeff
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-05-18 12:22:15 +0200

    add `extract-text-webpack-plugin` and packages upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yarn upgrade babel-plugin-react-css-modules enzyme enzyme-to-json neutrino neutrino-middleware-loader-merge neutrino-preset-airbnb-base neutrino-preset-jest neutrino-preset-react react-addons-test-utils react-test-renderer babel-polyfill
+yarn upgrade babel-plugin-react-css-modules enzyme enzyme-to-json neutrino neutrino-middleware-loader-merge neutrino-preset-airbnb-base neutrino-preset-jest neutrino-preset-react react-addons-test-utils react-test-renderer babel-polyfill extract-text-webpack-plugin
